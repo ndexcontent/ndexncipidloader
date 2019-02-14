21dc44c286bc115a7b22ce2e9927f386bd029eb9
Chris Churas <churas.camera@gmail.com>
2019-02-14 11:24:05 -0800

    first commit

---
+++ README.rst
@@ -0,0 +1,37 @@
+===========================
+NDEx NCI-PID content loader
+===========================
+
+
+.. image:: https://img.shields.io/pypi/v/ndexncipidloader.svg
+        :target: https://pypi.python.org/pypi/ndexncipidloader
+
+.. image:: https://img.shields.io/travis/coleslaw481/ndexncipidloader.svg
+        :target: https://travis-ci.org/coleslaw481/ndexncipidloader
+
+.. image:: https://readthedocs.org/projects/ndexncipidloader/badge/?version=latest
+        :target: https://ndexncipidloader.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+
+
+Loads NCI-PID data into NDEx
+
+
+* Free software: BSD license
+* Documentation: https://ndexncipidloader.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ 'ndex2==3.0.0a1',
+                 'ndexutil==0.1.0a2']
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Chris Churas",
+    author_email='contact@ndexbio.org',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Loads NCI-PID data into NDEx",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='ndexncipidloader',
+    name='ndexncipidloader',
+    packages=find_packages(include=['ndexncipidloader']),
+    scripts=[ 'ndexncipidloader/loadndexncipidloader.py'],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/coleslaw481/ndexncipidloader',
+    version='0.1.0',
+    zip_safe=False,
+)

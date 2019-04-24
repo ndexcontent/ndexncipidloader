320630f1bb0d06396a8f28154f61bc2637a6770b
Chris Churas <churas.camera@gmail.com>
2019-04-24 16:24:32 -0700

    updated to use 3.2.0 of ndex2 client

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'ndex2>=3.1.0a1,<=4.0.0',
+requirements = [ 'ndex2>=3.2.0,<=4.0.0',
                  'ndexutil>=0.1.0a3,<=1.0.0',
                  'biothings_client',
                  'ftpretty',
